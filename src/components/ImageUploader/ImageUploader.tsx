cc89b9b458da544317e7dd89b7573f2fe2b03161
blankumu35 <blankumu35@gmail.com>
2024-03-28 12:09:10 +0000

    Mini ImageUploader update

--- src/components/ImageUploader/ImageUploader.tsx
+++ src/components/ImageUploader/ImageUploader.tsx
@@ -16,22 +16,31 @@ interface ImageUploaderProps {
 const ImageUploader: React.FC<ImageUploaderProps> = ({ onClose, setFileNames }) => {
     const [images, setImages] = useState<ImageState[]>([]);
     const [fileList, setFileList] = useState<FileList | null>(null);
+    const [showWarning, setShowWarning] = useState(false);
+    const [checkedBrands, setCheckedBrands] = useState([]);
+    const [showBrandWarning, setShowBrandWarning] = useState(false);
+
     const api = UseAxios();
 
     const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
         if (e.target.files) {
             setFileList(e.target.files);
+            setShowWarning(false);
             const newImages = Array.from(e.target.files).map((file, index) => ({
                 url: URL.createObjectURL(file),
                 alt: `uploaded-${index}`,
                 file: file,
             }));
             setImages(prevImages => [...prevImages, ...newImages]);
+            if (images.length === 1) {
+                setShowWarning(false);
+            }
         }
     };
 
     const handleUpload = async () => {
-        if (fileList) {
+        if (images.length !== 0 && checkedBrands.length > 1) {
+            setShowWarning(false);
             let success = false;
             const formData = new FormData();
             images?.forEach((image: ImageState, index: number) => formData.append(`images[${index}]`, image?.file));
@@ -43,11 +52,27 @@ const ImageUploader: React.FC<ImageUploaderProps> = ({ onClose, setFileNames }) 
                 console.error(err);
             }
             alert(`Images uploaded ${success ? '' : 'un'}successfully!`);
+            onClose();
+        } else {
+            if (images.length === 0) {
+                setShowWarning(true);
+            } else {
+                setShowBrandWarning(true);
+            }
         }
-        onClose();
     };
 
-    const deleteImage = index => {
+    const handleCheckboxChange = (e: { target: { value: any; checked: any; }; }) => {
+        const brand = e.target.value;
+        if (e.target.checked) {
+            setCheckedBrands(prevCheckedBrands => [...prevCheckedBrands, brand]);
+            setShowBrandWarning(false);
+        } else {
+            setCheckedBrands(prevCheckedBrands => prevCheckedBrands.filter(item => item !== brand));
+        }
+    };
+
+    const deleteImage = (index: number) => {
         const updatedImages = images.filter((_, i) => i !== index);
         setImages(updatedImages);
     };
@@ -63,61 +88,104 @@ const ImageUploader: React.FC<ImageUploaderProps> = ({ onClose, setFileNames }) 
             <p style={{ marginBottom: -15 }}>Upload Multiple images along with the</p>
             <p>brands you would like us to detect:</p>
             <input type='file' accept='image/*' multiple onChange={handleFileChange} />
-            <div>
+            <p>Total Images: {images.length}</p>
+            <div className='image-container'>
                 {images.map((image, index) => (
-                    <div key={index} className='image-container'>
-                        <img
-                            src={image.url}
-                            alt={image.alt}
-                            style={{ width: '100px', margin: '10px' }}
-                            className='images'
-                        />
+                    <div key={index} className='image'>
                         <TiDeleteOutline
                             className='delete-icon'
-                            style={{ zIndex: 4, color: 'red', position: 'relative', left: 110, top: 10 }}
-                            onClick={() => deleteImage(index)} // Pass the index to the deleteImage function
+                            style={{
+                                zIndex: 4,
+                                color: 'red',
+                                position: 'relative',
+                                left: 93,
+                                top: 27,
+                                display: 'flex',
+                            }}
+                            onClick={() => deleteImage(index)}
+                        />
+                        <img
+                            src={image.url}
+                            key={index}
+                            alt={image.alt}
+                            style={{ width: '100px', margin: '10px', position: 'relative', display: 'flex' }}
+                            className='images'
                         />
                     </div>
                 ))}
             </div>
-            <p>Total Images: {images.length}</p>
             <div className='brand-checkbox'>
                 <p>Brands:</p>
-                <input type='checkbox' id='Nike' className='brand-checkbox1' value='Nike' />
-                <label htmlFor='Nike' id='Nike'>
-                    Nike
-                </label>
-                <input type='checkbox' id='Adidas' className='brand-checkbox2' value='Adidas' />
-                <label htmlFor='Adidas' id='Adidas'>
-                    Adidas
-                </label>
-                <input type='checkbox' id='Puma' className='brand-checkbox3' value='Puma' />
-                <label htmlFor='Puma' id='Puma'>
-                    Puma
-                </label>
-                <input type='checkbox' id='New-Balance' className='brand-checkbox4' value='New Balence' />
-                <label htmlFor='New Balance' id='New-Balance'>
-                    New Balance
-                </label>
+                <input
+                    type='checkbox'
+                    id='Nike'
+                    className='brand-checkbox1'
+                    value='Nike'
+                    onChange={handleCheckboxChange}
+                />
+                <label htmlFor='Nike'>Nike</label>
+                <input
+                    type='checkbox'
+                    id='Adidas'
+                    className='brand-checkbox2'
+                    value='Adidas'
+                    onChange={handleCheckboxChange}
+                />
+                <label htmlFor='Adidas'>Adidas</label>
+                <input
+                    type='checkbox'
+                    id='Puma'
+                    className='brand-checkbox3'
+                    value='Puma'
+                    onChange={handleCheckboxChange}
+                />
+                <label htmlFor='Puma'>Puma</label>
+                <input
+                    type='checkbox'
+                    id='New-Balance'
+                    className='brand-checkbox4'
+                    value='New Balance'
+                    onChange={handleCheckboxChange}
+                />
+                <label htmlFor='New Balance'>New Balance</label>
                 <br />
 
-                <input type='checkbox' id='Lululemon' className='brand-checkboxes' value='Lululemon' />
-                <label htmlFor='Lululemon' id='Lululemon'>
-                    Lululemon
-                </label>
-                <input type='checkbox' id='Reebok' className='brand-checkboxes' value='Reebok' />
-                <label htmlFor='Reebok' id='Reebok'>
-                    Reebok
-                </label>
-                <input type='checkbox' id='North-Face' className='brand-checkboxes' value='North Face' />
-                <label htmlFor='North Face' id='North-Face'>
-                    North Face
-                </label>
-                <input type='checkbox' id='Under-Armor' className='brand-checkboxes' value='Under Armor' />
-                <label htmlFor='Under Armor' id='Under-Armor'>
-                    Under Armor
-                </label>
+                <input
+                    type='checkbox'
+                    id='Lululemon'
+                    className='brand-checkboxes'
+                    value='Lululemon'
+                    onChange={handleCheckboxChange}
+                />
+                <label htmlFor='Lululemon'>Lululemon</label>
+                <input
+                    type='checkbox'
+                    id='Reebok'
+                    className='brand-checkboxes'
+                    value='Reebok'
+                    onChange={handleCheckboxChange}
+                />
+                <label htmlFor='Reebok'>Reebok</label>
+                <input
+                    type='checkbox'
+                    id='North-Face'
+                    className='brand-checkboxes'
+                    value='North Face'
+                    onChange={handleCheckboxChange}
+                />
+                <label htmlFor='North Face'>North Face</label>
+                <input
+                    type='checkbox'
+                    id='Under-Armor'
+                    className='brand-checkboxes'
+                    value='Under Armor'
+                    onChange={handleCheckboxChange}
+                />
+                <label htmlFor='Under Armor'>Under Armor</label>
             </div>
+            {showWarning && <p style={{ display: 'block', color: 'red' }}>Please select an image(s)</p>}
+            {showBrandWarning && <p style={{ display: 'block', color: 'red' }}>Please select a brand(s)</p>}
+            {/*<p>Checked brands: {checkedBrands.join(', ')}</p>*/}
             <button className='upload-button' onClick={handleUpload}>
                 Upload Images
             </button>
