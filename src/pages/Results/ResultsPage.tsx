27b6822c8d1b9a93096b5ef053609b2a7b9c8b4a
Sam <sktylr@gmail.com>
2024-04-09 00:13:36 +0100

    hacky solution to render page differently depending on number of images

--- src/pages/Results/ResultsPage.tsx
+++ src/pages/Results/ResultsPage.tsx
@@ -8,28 +8,35 @@ import { Detection } from '../../types/interfaces';
 const ResultsPage: React.FC = () => {
     const { detectionResults } = useDetectionResults();
 
-    return (
-        <div style={{ width: '100vw' }}>
-            <h1 style={{ marginTop: '4em' }}>Detection Results</h1>
-            <Container maxWidth='lg'>
-                {detectionResults.map((result, index) => {
-                    const getBoundingBoxes = () => {
-                        const detections = Object.values(result?.analysis?.detections);
-                        return detections?.flatMap(detection =>
-                            detection?.map((detection: Detection) => detection?.bbox),
-                        );
-                    };
+    const length = detectionResults?.length ?? 0;
 
-                    return (
-                        <Grid item xs={12} xl={12} key={index}>
-                            <IndividualResult
-                                boundingBoxes={getBoundingBoxes()}
-                                image={{ src: result?.image?.source }}
-                            />
-                        </Grid>
-                    );
-                })}
-            </Container>
+    const h1ClassName = length > 1 ? 'results-heading' : '';
+    const containerClassName = length > 2 ? 'container-wrapper' : length === 2 ? 'container-wrapper-double' : '';
+
+    return (
+        <div style={{ width: '100vw', paddingTop: '4em' }}>
+            <h1 className={h1ClassName}>Detection Results</h1>
+            <div className={containerClassName}>
+                <Container maxWidth='lg' className='results-container'>
+                    {detectionResults.map((result, index) => {
+                        const getBoundingBoxes = () => {
+                            const detections = Object.values(result?.analysis?.detections);
+                            return detections?.flatMap(detection =>
+                                detection?.map((detection: Detection) => detection?.bbox),
+                            );
+                        };
+
+                        return (
+                            <Grid item xs={12} xl={12} key={index}>
+                                <IndividualResult
+                                    boundingBoxes={getBoundingBoxes()}
+                                    image={{ src: result?.image?.source }}
+                                />
+                            </Grid>
+                        );
+                    })}
+                </Container>
+            </div>
         </div>
     );
 };
