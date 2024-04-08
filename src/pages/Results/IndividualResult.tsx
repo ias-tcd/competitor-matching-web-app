bcc234657c52b6c85a321d2dd3eba0d72bb91149
Sam <sktylr@gmail.com>
2024-04-08 11:52:33 +0100

    render identified brands in list

--- src/pages/Results/IndividualResult.tsx
+++ src/pages/Results/IndividualResult.tsx
@@ -1,4 +1,4 @@
-import { Card, Grid } from '@mui/material';
+import { Card, Grid, Typography, List, ListItem, ListItemText } from '@mui/material';
 import CardContent from '@mui/material/CardContent';
 import { BBox as IBoundingBox } from '../../types/interfaces';
 import BoundingBoxes from '../../components/BoundingBoxes/BoundingBoxes';
@@ -13,6 +13,10 @@ interface IndividualResultProps {
 }
 
 const IndividualResult = ({ image, boundingBoxes }: IndividualResultProps) => {
+    const brandNames = boundingBoxes?.filter(bbox => !!bbox?.brand)?.map(bbox => bbox?.brand);
+
+    console.log(brandNames);
+
     return (
         <Card variant='outlined' sx={{ mb: '1em' }}>
             <CardContent>
@@ -20,9 +24,19 @@ const IndividualResult = ({ image, boundingBoxes }: IndividualResultProps) => {
                     <Grid item sx={{ maxWidth: '20rem', maxHeight: '20rem' }}>
                         <BoundingBoxes boundingBoxes={boundingBoxes} image={image} colours={['yellow']} />
                     </Grid>
-                    {/* <Grid>
-                        <Typography variant='h4'>Lorem ipsum</Typography>
-                    </Grid> */}
+                    <Grid item sx={{ padding: '0.5em' }}>
+                        <Typography variant='h5'>Brands Identified</Typography>
+                        <List>
+                            {brandNames?.map(name => (
+                                <ListItem key={name}>
+                                    <ListItemText
+                                        primary={name}
+                                        primaryTypographyProps={{ variant: 'body1', color: 'black' }}
+                                    />
+                                </ListItem>
+                            ))}
+                        </List>
+                    </Grid>
                 </Grid>
             </CardContent>
         </Card>
